a7022cf4aa0b0e7a466542f1cee76d4c82787d69
raydoom <raydoom@163.com>
2017-03-14 11:04:53 +0800

    1.0
    get baidu_sou result (url and website link)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python 
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 import urllib
 
@@ -19,12 +19,56 @@ def download(urld,file):
 def progress(blk,blk_size,total_size):
         print('%dkb/%dkb = %.02f%%' % (blk*blk_size,total_size,(float)(blk*blk_size)*100/total_size))
 
+def baidu_sou(wd):
+	try:
+		url = 'http://www.baidu.com/s?'
+		wd = {'wd':wd}
+		r = requests.get(url,params=wd)
+		r.raise_for_status()
+#		r.encoding = r.apparent_encoding
+		r.encoding = 'utf-8'
+		return r.text		
+	except:
+		print ('baidu_sou failed')
 
+def get_burl(html):
+	soup = BeautifulSoup(html,'html.parser')
+	print ('numbers of result:' ,len(soup.findAll(name = 'h3')))
+	
+	h = (soup.findAll(name = 'h3'))
+#	s = str(h[4]).split('href=')
+	burl = []
+	for x in range(len(soup.findAll(name = 'h3'))):
+	
+		s = (str(h[x]).split('href=')[1].split()[0])
+#		print  (str(h[x]).split('href=')[1].split()[0])
+		link = s.replace('"','')
+		burl.append(link)
 
+#	print (burl)
+	return burl
+		
+def get_real_url_name(burl):
+	l = len(burl)
+	result = []
+	for x in range(l):
+		s = requests.get(burl[x])
+#		print (s.url)
+		s.encoding = s.apparent_encoding
+		soup = BeautifulSoup(s.text,'html.parser')
+#		print (soup.title)
+		result.append(soup.url)
+		result.append(soup.title)
+	return result
+        
+def display(result):
+	for x in range(len(result)):
+		print (result[x])
+		
 if __name__ == "__main__":
-#       url = raw_input("input the url:")
-        url = "https://www.baidu.com"
-#       ma.open(url)
-#       download("http://mirrors.hust.edu.cn/apache//ant/binaries/apache-ant-1.10.1-bin.tar.gz","apache-ant-1.10.1-bin.tar.gz")
-        s=ma()
-        s.open(url)
+	
+	html = baidu_sou('zrbao')
+	burl = get_burl(html)
+	url = get_real_url_name(burl)
+	get_real_url_name(burl)
+	display(url)
